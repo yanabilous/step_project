04cbfd4a69e916009899c9d71af29ee2549e4f52
yanabilous <yanabilous05@gmail.com>
2022-09-26 11:38:32 +0300

    carousel

--- js/index.js
+++ js/index.js
@@ -24,12 +24,9 @@ btn.addEventListener("click", function () {
 document.querySelector(".checkbox_section").addEventListener("click", function (event) {
     if (event.target && event.target.matches("input[name='domain']")) {
         const checkValue = document.querySelector("input[name=\"domain\"]:checked").value;
-        console.log(checkValue);
         const cards = document.querySelectorAll(".card");
         cards.forEach((el, index) => {
-            // el.style.display = el.classList.contains(checkValue) ? "block" : "none";
-
-            if (el.classList.contains(checkValue) || checkValue==='all') {
+            if (el.classList.contains(checkValue) || checkValue === "all") {
                 el.style.display = "block";
             } else {
                 el.style.display = "none";
@@ -37,5 +34,65 @@ document.querySelector(".checkbox_section").addEventListener("click", function (
         });
     }
 });
+// __________________________________
+
+let slider = document.querySelector(".carousel_img");
 
 
+slider.addEventListener("click", function (ev) {
+    if (ev.target.classList.contains("slider_img")) {
+        document.querySelector(".carousel_img img.active").classList.remove("active");
+        document.querySelector(".testimonial.active").classList.remove("active");
+        document.querySelector(`#${ev.target.dataset.imgid}`).classList.add("active");
+        ev.target.classList.add("active");
+
+    }
+});
+
+
+const imgLength = document.querySelectorAll(".slider_img").length;
+let imagesSlider = document.querySelectorAll(".carousel_img img");
+document.querySelector(".arrowLeft").style.display = "none";
+
+document.querySelector(".arrowLeft").addEventListener("click", function (ev) {
+    for (let i = 0; i < imagesSlider.length; i++) {
+        if (imagesSlider[i].classList.contains("active")) {
+            imagesSlider[i].classList.remove("active");
+            imagesSlider[i - 1].classList.add("active");
+            document.querySelector(".testimonial.active").classList.remove("active");
+            document.querySelector(`.testimonial:nth-child(${i})`).classList.add("active");
+            toggleArrows(i - 1);
+            return;
+
+        }
+
+    }
+});
+
+
+document.querySelector(".arrowRight").addEventListener("click", function (ev) {
+    for (let i = 0; i < imagesSlider.length; i++) {
+        if (imagesSlider[i].classList.contains("active")) {
+            imagesSlider[i].classList.remove("active");
+            imagesSlider[i + 1].classList.add("active");
+            document.querySelector(".testimonial.active").classList.remove("active");
+            document.querySelector(`.testimonial:nth-child(${i + 2})`).classList.add("active");
+            toggleArrows(i + 1);
+            return;
+        }
+    }
+});
+
+
+function toggleArrows(i) {
+    if (i === 0) {
+        document.querySelector(".arrowLeft").style.display = "none";
+        document.querySelector(".arrowRight").style.display = "block";
+    } else if (i === imgLength - 1) {
+        document.querySelector(".arrowRight").style.display = "none";
+        document.querySelector(".arrowLeft").style.display = "block";
+    } else {
+        document.querySelector(".arrowLeft").style.display = "block";
+        document.querySelector(".arrowRight").style.display = "block";
+    }
+}
